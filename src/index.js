73eecb43582c92ebf051d9b89080ed5638b82e27
Kamehamejaime <jaime.catalanc@gmail.com>
2023-11-28 02:57:14 -0300

    Implementación de bases de datos y vista colaboradores

--- src/index.js
+++ src/index.js
@@ -1,24 +1,68 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-
 import indexRoutes from "./routes/index.js";
+import mysql from "mysql";
+import { error, log } from "console";
 
+//const { default: morgan } = await import("morgan");
 
 
 const app = express();
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set("views", join(__dirname, "views"));
+
+//SETTINGS
+const PORT = process.env.PORT || 3000;
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-console.log(join(__dirname, "views"));
+app.set("views", join(__dirname, "views"));
 
 app.use(indexRoutes);
-app.use(express.static(join(__dirname,'public')));
+app.use(express.static(join(__dirname, "public")));
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+//Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "camilacote",
+});
+
+//Establecer conexión
+connection.connect((error) => {
+  if (error) {
+    console.log("error al conectar a la base de datos", error);
+    return;
+  }
+  console.log("Conexión exitosa a la base de datos");
+});
+
+app.get('/colaboradores', (req, res) => {
+    connection.query('SELECT * FROM colaboradores', (err, rows) => {
+       if (err) throw err;
+   
+       res.render('colaboradores', { colaboradores: rows });
+    });
+   });
 
 
+   //Insertar datos en la tabla colaboradores
+   app.post('/colaboradores', (req, res) => {
+    const { name, position } = req.body;
+   
+    connection.query('INSERT INTO Colaboradores SET ?',
+       {
+         name: name,
+         position: position
+       },
+       (err, rows) => {
+         if (err) throw err;
+   
+         res.render('colaboradores', { colaboradores: rows, title:'Colaboradores'});
+       }
+    );
+   });
+   
 
+app.listen(PORT, () => console.log("Servidor escuchando en puerto {PORT}"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,26 @@ router.get("/", (req, res) =>
   res.render("index", { title: "Pagina principal" })
 );
 
-router.get("/about", (req, res) => res.render("about"));
+router.get("/about", (req, res) => res.render("about", { title: "About" }));
 
 router.get("/contact", (req, res) => res.render("contact"));
 
 router.get("public/css", (req, res) => res.sendFile("style.css"));
 
+
 export default router;
+
+/*
+router.get("/colaboradores", (req, res) => {
+  connection.query("SELECT *FROM Colaboradores", (error, results) => {
+    if (error) {
+      console.log("Error al consultar los datos de la tabla Colaboradores",error);
+      return;
+    }
+
+    res.render("colaboradores", { colaboradores: results });
+  });
+});
+
+
+*/
